558a796bd6f17afefaf8da620b3cfd587b23cbe2
LVSHIWEN123 <lvshiwen08@163.com>
2019-07-21 17:52:31 -0700

    project

--- Asset/JavaScript/TrainTime.js
+++ Asset/JavaScript/TrainTime.js
@@ -1,80 +1,109 @@
-$(document).ready(function () {
-    // Initialize the Firebase
-    var firebaseConfig = {
-        apiKey: "AIzaSyD9rXSz6avji4ARjAS0R0LKWGWNIZsFmmY",
-        authDomain: "traintimefinal.firebaseapp.com",
-        databaseURL: "https://traintimefinal.firebaseio.com",
-        projectId: "traintimefinal",
-        storageBucket: "traintimefinal.appspot.com",
-        messagingSenderId: "992012568712",
-        appId: "1:992012568712:web:ef9775b585afc986"
+$(document).ready(function() {
+  // Initialize the Firebase
+  var firebaseConfig = {
+    apiKey: "AIzaSyD9rXSz6avji4ARjAS0R0LKWGWNIZsFmmY",
+    authDomain: "traintimefinal.firebaseapp.com",
+    databaseURL: "https://traintimefinal.firebaseio.com",
+    projectId: "traintimefinal",
+    storageBucket: "traintimefinal.appspot.com",
+    messagingSenderId: "992012568712",
+    appId: "1:992012568712:web:ef9775b585afc986"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
+
+  // Send the data to the firebase once the submit button has been clicked
+  $("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+
+    var name = $("#train-name-input")
+      .val()
+      .trim();
+    var destination = $("#destination-input")
+      .val()
+      .trim();
+    var start = $("#start-input")
+      .val()
+      .trim();
+    var frequency = $("#frequency-input")
+      .val()
+      .trim();
+
+    let train = {
+      name,
+      destination,
+      start,
+      frequency
     };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-    var database = firebase.database();
+    database.ref("/trainList").push(train);
+  });
 
+  database.ref("/trainList").on("child_added", function(snapshot) {
+    let train = snapshot.val();
+    var StartHour = parseInt(train.start.slice(0, 2));
+    var StartMinutes = parseInt(train.start.slice(-2));
+    var CurrentHour = parseInt(moment().hours());
+    var CurrentMinutes = parseInt(moment().minutes());
 
-    // Send the data to the firebase once the submit button has been clicked 
-    $('#add-train-btn').on('click', function (event) {
-        event.preventDefault();
+    var hourDiff;
+    var minutesDiff;
+    var timeDiff;
+    // Time away
+    var timeAway;
+    var ArrivialMinutes;
+    // arrivialTime
+    var arrivialTime;
 
-        var name = $('#train-name-input').val().trim();
-        var destination = $('#destination-input').val().trim();
-        var start = $('#start-input').val().trim();
-        var frequency = $('#frequency-input').val().trim();
+    // Come after the first train
+    if (
+      CurrentHour > StartHour ||
+      (CurrentHour == StartHour && CurrentMinutes >= StartMinutes)
+    ) {
+      // Get how many minutes has gone from the first train
+      hourDiff = CurrentHour - StartHour;
+      minutesDiff = CurrentMinutes - StartMinutes;
+      timeDiff = hourDiff * 60 + minutesDiff;
 
-        let train = {
-            name,
-            destination,
-            start,
-            frequency
-        }
-        database.ref("/trainList").push(train);
-    })
+      // The % value is how much time has gone from the last train then we know how much time for the next train
+      timeAway =
+        parseInt(train.frequency) - (timeDiff % parseInt(train.frequency));
+      ArrivialMinutes = CurrentMinutes + timeAway;
+    }
+    // Come before the first train
+    if (
+      CurrentHour < StartHour ||
+      (CurrentHour == StartHour && CurrentMinutes < StartMinutes)
+    ) {
+      hourDiff = StartHour - CurrentHour;
+      minutesDiff = StartMinutes - CurrentMinutes;
+      timeDiff = hourDiff * 60 + minutesDiff;
 
-    database.ref("/trainList").on('child_added', function (snapshot) {
-        let train = snapshot.val();
-        var StartHour = parseInt(train.start.slice(0, 2));
-        var StartMinutes = parseInt(train.start.slice(-2));
-        var CurrentHour = parseInt(moment().hours());
-        var CurrentMinutes = parseInt(moment().minutes());
+      timeAway = timeDiff;
+      CurrentHour = StartHour;
+      ArrivialMinutes = StartMinutes;
+    }
+    
+    if (CurrentHour)
+      if (timeAway < 10) {
+        timeAway = "0" + timeAway;
+      }
+    if (ArrivialMinutes >= 60) {
+      CurrentHour = CurrentHour + 1;
+      ArrivialMinutes = ArrivialMinutes - 60;
+    }
+    if (CurrentHour >= 24) {
+      CurrentHour = CurrentHour - 24;
+    }
+    if (CurrentHour < 10) {
+      CurrentHour = "0" + CurrentHour;
+    }
+    if (ArrivialMinutes < 10) {
+      ArrivialMinutes = "0" + ArrivialMinutes;
+    }
+    arrivialTime = CurrentHour + ":" + ArrivialMinutes;
 
-        console.log('Start Hour' + StartHour);
-        console.log('Start Minutes' + StartMinutes);
-
-        console.log('Current Hour' + CurrentHour);
-        console.log('Current Minutes' + CurrentMinutes);
-        // Time Length
-        var hourDiff;
-        var minutesDiff;
-        var timeDiff;
-        // Time away
-        var timeAway
-        var ArrivialMinutes
-        // arrivialTime
-        var arrivialTime;
-
-        hourDiff = CurrentHour - StartHour;
-        minutesDiff = CurrentMinutes - StartMinutes;
-        timeDiff = hourDiff * 60 + minutesDiff;
-
-        console.log(timeDiff);
-        timeAway = parseInt(train.frequency) - timeDiff % (parseInt(train.frequency));
-        console.log(timeAway);
-        ArrivialMinutes = CurrentMinutes + timeAway;
-        if (timeAway < 10) {
-            timeAway = '0' + timeAway;
-        }
-        if (ArrivialMinutes >= 60) {
-            CurrentHour = CurrentHour + 1;
-            ArrivialMinutes = ArrivialMinutes - 60;
-        }
-        if (ArrivialMinutes < 10) {
-            ArrivialMinutes = '0' + ArrivialMinutes;
-        }
-        arrivialTime = CurrentHour + ':' + ArrivialMinutes;
-
-        let trainHTML = `
+    let trainHTML = `
 <tr>
 <td scope="col">${train.name}</td>
 <td scope="col">${train.destination}</td>
@@ -84,8 +113,6 @@ $(document).ready(function () {
 </tr>
 `;
 
-        $("#train-table tbody").append(trainHTML);
-
-
-    })
-})
+    $("#train-table tbody").append(trainHTML);
+  });
+});
